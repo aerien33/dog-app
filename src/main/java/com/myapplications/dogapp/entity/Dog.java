b71abb9e475a511ef85fe04f4f832583cc9be44c
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2023-11-14 10:52:15 +0100

    Add Dog entity class

---
+++ src/main/java/com/myapplications/dogapp/entity/Dog.java
@@ -0,0 +1,37 @@
+package com.myapplications.dogapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "dogs")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "owner", nullable = false)
+    private String owner;
+
+    @Column(name = "breed")
+    private String breed;
+
+    @Column(name = "birthday", nullable = false)
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date birthday;
+
+}
